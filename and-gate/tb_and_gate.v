103f8c6bef04f119fc536382602c531c69092a6b
davidvarshanidze <davidvarshanidze1@gmail.com>
2024-10-11 17:34:01 +0400

    ^and&

---
+++ and-gate/tb_and_gate.v
@@ -0,0 +1,25 @@
+module tb_and_gate;
+
+    reg a;
+    reg b;
+    wire y;
+
+    and_gate uut (
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+
+    initial begin
+        $dumpfile("and_waveform.vcd");
+        $dumpvars(0, tb_and_gate);
+        $monitor("a = %b, b = %b, y = %b", a, b, y);
+
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+
+        $finish;
+    end
+endmodule
--- xor-gate/xor_gate.v
+++ xor-gate/xor_gate.v
@@ -1,7 +1,6 @@
-module xor_gate(
-    input a,
-    input b,
-    output y
-);
-    assign y = a ^ b;
+module xor_gate(y, a, b);
+output y;
+input a, b;
+
+assign y = a ^ b;
 endmodule
